a0b5847790d740350a72d4405454ba4c4e60e73a
Dev.Calany <devcalany@gmail.com>
2026-07-01 21:53:45 -0300

    #

---
+++ script.js
@@ -0,0 +1,12 @@
+const btn = document.getElementById('backToTop');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 40) {
+    btn.classList.add('show');
+  } else {
+    btn.classList.remove('show');
+  }
+});
+btn.addEventListener('click', () => {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+});
